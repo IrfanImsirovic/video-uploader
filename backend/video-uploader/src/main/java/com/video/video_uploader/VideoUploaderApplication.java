b2b841a132149b24e76d2cfd7238a484dd1973d4
Irfan Imširović <152695093+IrfanImsirovic@users.noreply.github.com>
2026-01-23 23:58:23 +0100

    Initial project setup (Spring Boot + React + Postgres)

---
+++ backend/video-uploader/src/main/java/com/video/video_uploader/VideoUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.video.video_uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoUploaderApplication.class, args);
+	}
+
+}
---
+++ backend/video-uploader/src/test/java/com/video/video_uploader/VideoUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.video_uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
