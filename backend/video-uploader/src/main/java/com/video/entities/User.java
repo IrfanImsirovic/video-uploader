6cd98292b7b6e3895f8a34c1be80e2b18a48ff74
irfan imsirovic <irfan.imsirovic999@gmail.com>
2026-01-26 18:40:18 +0100

    Created the User entity

--- backend/video-uploader/src/main/java/com/video/entities/User.java
+++ backend/video-uploader/src/main/java/com/video/entities/User.java
@@ -1,5 +1,46 @@
 package com.video.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Size(min=3, max=32, message = "Username can't exceed 32 characters!")
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    @Email(message = "Email should be valid!")
+    private String email;
+    @Size(min=8, max = 32, message = "Password must be between 8 and 32 characters!")
+    @Column(nullable = false, length = 255)
+    @JsonIgnore
+    private String password;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
