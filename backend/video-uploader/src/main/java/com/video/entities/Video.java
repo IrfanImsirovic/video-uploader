cdc658946a01cce2f59de0968a160d0dc7c3ce5b
Irfan Imširović <152695093+IrfanImsirovic@users.noreply.github.com>
2026-01-28 13:09:47 +0100

    Video and Raw Files Entities Created

---
+++ backend/video-uploader/src/main/java/com/video/entities/Video.java
@@ -0,0 +1,63 @@
+package com.video.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "videos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(length = 500, columnDefinition = "text")
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isPrivate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uploader_id", nullable = false)
+    private User uploader;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "video_file_id", nullable = false)
+    private RawFile videoFile;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "thumbnail_file_id")
+    private RawFile thumbnailFile;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
